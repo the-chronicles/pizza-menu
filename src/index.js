2b5fe11ab25d5e3d07ca147de330d7b10ee24712
the-chronicles <oluseungideon@gmail.com>
2023-10-11 16:40:26 +0100

    Commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 function App() {
   return <h1>Hello React!</h1>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
